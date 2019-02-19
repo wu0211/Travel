9f22f000e07783d44bb2dea2a9b4ca31c316094c
wubosong <wbs_211@163.com>
2019-02-19 11:27:36 +0800

    update

---
+++ Travel/src/pages/home/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+
+</script>
+
+<style lang="stylus" scoped>
+
+  @import '~styles/varibles.styl';
+
+  .header {
+    line-height: 43px;
+    display: flex;
+    color: #fff;
+    background: $bgColor;
+  }
+
+  .header-left {
+    width: 30px;
+    float: left;
+
+  }
+
+  .back-icon {
+    text-align: center;
+    font-size: 20px;
+  }
+
+  .header-input {
+    background: #fff;
+    border-radius: 5px;
+    margin-top: 6px;
+    height: 32px;
+    margin-left: 4px;
+    flex: 1;
+    color: #ccc;
+    line-height: 32px;
+    padding-left: 4px;
+  }
+
+  .header-right {
+    width: 62px;
+    float: right;
+    text-align: center;
+  }
+
+  .arrow-icon {
+    margin-left: -2px;
+    font-size: 12px;
+  }
+
+</style>
