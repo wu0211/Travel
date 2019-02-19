e80c3617fef977549118b41891dcf796ec55807e
wubosong <wbs_211@163.com>
2019-02-19 12:20:41 +0800

    Update swiper.vue

--- Travel/src/pages/home/components/swiper.vue
+++ Travel/src/pages/home/components/swiper.vue
@@ -11,33 +11,32 @@
 </template>
 
 <script>
-  export default {
-    name: "HomeSwiper",
-    data() {
-      return {
-        swiperOption: {
-          pagination: '.swiper-pagination',
-          loop: true
-        },
-        swiperList: [{
-          id: "0001",
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/70/5603e6a535062402.jpg_750x200_2c552f2c.jpg'
-        }, {
-          id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/d1/cd4f32e924a5a302.jpg_750x200_3d492837.jpg'
-        }]
-      }
+export default {
+name: "HomeSwiper",
+data() {
+    return {
+    swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+    },
+    swiperList: [{
+        id: "0001",
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/70/5603e6a535062402.jpg_750x200_2c552f2c.jpg'
+    }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/d1/cd4f32e924a5a302.jpg_750x200_3d492837.jpg'
+    }]
     }
-  }
+    }
+}
 
 </script>
 
-<style>
+<style lang="stylus" scoped>
   .wrapper >>> .swiper-pagination-bullet-active {
       
       background: #fff !important;
   }
-
 
   .wrapper {
     width: 100%;
