51307da5232b50707a34cfe6f6816c14fbc873e7
wubosong <wbs_211@163.com>
2019-02-20 10:50:53 +0800

    city-search

---
+++ Travel/src/pages/city/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "CitySearch"
+  }
+
+</script>
+
+<style lang="stylus" scoped>
+    .search{
+        height: 31px;
+        background: #00bcd4;
+        padding: 0 3px;
+    }
+    .search-input{
+        width: 100%;
+        height: 28px;
+        line-height: 28px;
+        text-align: center;
+        border-radius: .06rem;
+        color: #666;
+        padding: 0 0.1rem;
+        box-sizing: border-box;
+    }
+
+</style>
