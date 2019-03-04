a17bb24ca61b201ac46b09ae7926cce8be50a0e8
wubosong <wbs_211@163.com>
2019-03-04 15:48:08 +0800

    ·

--- Travel/src/App.vue
+++ Travel/src/App.vue
@@ -2,26 +2,29 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    
+
     <!-- 显示的是当前路由地址所对应的内容 -->
     <keep-alive exclude="Detail">
       <router-view />
-    
+
     </keep-alive>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  created() {
-    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-    //  alert('手机端')
-   }else{
-      //alert('PC端')
-   }
-  },
-}
+  export default {
+    name: 'App',
+    created() {
+      if ((navigator.userAgent.match(
+          /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+        ))) {
+        //  alert('手机端')
+      } else {
+        //alert('PC端')
+      }
+    },
+  }
+
 </script>
 
 <style>
